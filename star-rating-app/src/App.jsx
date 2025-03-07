ac948c1d88b0c30b830b3197c6440ea3dc7a4ee2
Vishal Rathod <vrathod591@gmail.com>
2025-03-07 18:40:28 +0530

    Star rating component created

--- star-rating-app/src/App.jsx
+++ star-rating-app/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import StarRatings from './components/StarRatings';
 
 function App() {
 
   return (
     <>
       <h1 className='font-bold text-center mt-4'>Star Rating App</h1>
+      <StarRatings />
     </>
   )
 }
