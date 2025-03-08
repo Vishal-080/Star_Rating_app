dfd1fcb1445b8df44da8781e38691941fef0cb10
Vishal Rathod <vrathod591@gmail.com>
2025-03-09 00:52:35 +0530

    star rating coding

--- star-rating-app/src/components/StarRatings.jsx
+++ star-rating-app/src/components/StarRatings.jsx
@@ -1,11 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const StarRatings = ({totalStars = 5}) => {
-
-
-  return (
-    <div>StarRatings ★</div>
-  )
-}
+const StarRatings = ({ totalStars = 5 }) => {
+  return <div>StarRatings ★</div>;
+};
 
 export default StarRatings;
