b89fdc7ee4b817d47333d0e8af2a5f5bad633cf4
Vishal Rathod <vrathod591@gmail.com>
2025-03-19 21:28:43 +0530

    Coding

--- star-rating-app/src/components/StarRatings.jsx
+++ star-rating-app/src/components/StarRatings.jsx
@@ -8,6 +8,22 @@ const StarRatings = ({ totalStars = 5 }) => {
     <div className="container flex justify-center items-center">
       <h2 className="font-bold text-2xl">Star Rating Feature</h2>
       <div>⭐⭐⭐⭐⭐⭐</div>
+      <div className="stars">
+        {[...Array(totalStars)].map((_, index) => (
+          <span
+            key={index}
+            onClick={() => setRating(index + 1)}
+            onMouseEnter={() => setHover(index + 1)}
+            onMouseLeave={() => setHover(0)}
+            className="star"
+            style={{
+              color: index < (hover || rating) ? "yellow" : "#ccc",
+            }}
+          >
+            ★
+          </span>
+        ))}
+      </div>
       <p className="text-center">
         Rating is:- {rating}/{totalStars}
       </p>
