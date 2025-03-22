521427b27f96b07fd4441174ed288a4b3c0d2b89
Vishal Rathod <vrathod591@gmail.com>
2025-03-22 20:27:47 +0530

    Counter app

--- star-rating-app/src/components/Counter.jsx
+++ star-rating-app/src/components/Counter.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Counter = () => {
   return (
-    <div>Counter</div>
+    <div className='border border-amber-300 w-6/12 m-auto text-center'>
+        <h1>Counter App</h1>
+    </div>
   )
 }
 
