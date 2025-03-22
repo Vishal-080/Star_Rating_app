295d7a6c295bf58ba6e62ab52c1c6097edbfdf36
Vishal Rathod <vrathod591@gmail.com>
2025-03-23 00:24:20 +0530

    Counter app coding

--- star-rating-app/src/components/Counter.jsx
+++ star-rating-app/src/components/Counter.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
+
+    const [count, setCount] = useState(0);
+
   return (
     <div className="border border-amber-300 w-6/12 m-auto text-center my-8">
       <h1 className="font-medium text-xl">Counter App</h1>
-      <p>{Count}</p>
+      <p>{count}</p>
       <div className="my-4">
         <button className="border p-2 rounded-lg mx-2">Add ➕</button>
         <button className="border p-2 rounded-lg mx-2">Substract ➖</button>
