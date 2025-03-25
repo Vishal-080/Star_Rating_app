0fd670afa2613a956c63b633c0df34f1f429e715
Vishal Rathod <vrathod591@gmail.com>
2025-03-25 13:07:29 +0530

    counter coding

--- star-rating-app/src/components/Counter.jsx
+++ star-rating-app/src/components/Counter.jsx
@@ -7,6 +7,12 @@ const Counter = () => {
     setCount(count+1);
   }
 
+  const handleSubsCount = () => {
+   count>0 ?  setCount(count-1) : "";
+  }
+
+  
+
   return (
     <div className="border border-amber-300 w-6/12 m-auto text-center my-8">
       <h1 className="font-medium text-xl">Counter App</h1>
@@ -15,7 +21,7 @@ const Counter = () => {
         <button className="border p-2 rounded-lg mx-2 cursor-pointer" onClick={handleAddCount}>
           Add ➕
         </button>
-        <button className="border p-2 rounded-lg mx-2 cursor-pointer">
+        <button className="border p-2 rounded-lg mx-2 cursor-pointer" onClick={handleSubsCount}>
           Substract ➖
         </button>
         <button className="border p-2 rounded-lg mx-2 cursor-pointer">
