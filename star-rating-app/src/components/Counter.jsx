9926157095624da9727c98c48217a16a484ef6b0
Vishal Rathod <vrathod591@gmail.com>
2025-03-25 13:10:37 +0530

    counter coding completed

--- star-rating-app/src/components/Counter.jsx
+++ star-rating-app/src/components/Counter.jsx
@@ -4,30 +4,38 @@ const Counter = () => {
   const [count, setCount] = useState(0);
 
   const handleAddCount = () => {
-    setCount(count+1);
-  }
+    setCount(count + 1);
+  };
 
   const handleSubsCount = () => {
-   count>0 ?  setCount(count-1) : "";
-  }
+    count > 0 ? setCount(count - 1) : "";
+  };
 
   const handleReset = () => {
     setCount(0);
-  }
-
+  };
 
   return (
     <div className="border border-amber-300 w-6/12 m-auto text-center my-8">
       <h1 className="font-medium text-xl">Counter App</h1>
       <p>{count}</p>
       <div className="my-4">
-        <button className="border p-2 rounded-lg mx-2 cursor-pointer" onClick={handleAddCount}>
+        <button
+          className="border p-2 rounded-lg mx-2 cursor-pointer"
+          onClick={handleAddCount}
+        >
           Add ➕
         </button>
-        <button className="border p-2 rounded-lg mx-2 cursor-pointer" onClick={handleSubsCount}>
+        <button
+          className="border p-2 rounded-lg mx-2 cursor-pointer"
+          onClick={handleSubsCount}
+        >
           Substract ➖
         </button>
-        <button className="border p-2 rounded-lg mx-2 cursor-pointer" onClick={handleReset}>
+        <button
+          className="border p-2 rounded-lg mx-2 cursor-pointer"
+          onClick={handleReset}
+        >
           Reset 🔴
         </button>
       </div>
