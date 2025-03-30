5d6bac695804651911ded5fbabcac8997228dbde
Vishal Rathod <vrathod591@gmail.com>
2025-03-31 01:08:01 +0530

    Lazy loading implemented in Login component

--- star-rating-app/src/App.jsx
+++ star-rating-app/src/App.jsx
@@ -1,11 +1,18 @@
 import StarRatings from "./components/StarRatings";
 import Counter from "./components/Counter";
+import Login from "./components/Login";
+import { lazy, Suspense } from "react";
 
 function App() {
+  const Login = lazy(() => import("./components/Login"));
+
   return (
     <>
       <StarRatings />
       <Counter />
+      <Suspense fallback="Loading...!">
+        <Login />
+      </Suspense>
     </>
   );
 }
