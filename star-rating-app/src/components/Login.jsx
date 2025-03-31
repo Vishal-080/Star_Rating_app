8d938829349e57481122fd8f3a46760ead3987a3
Vishal Rathod <vrathod591@gmail.com>
2025-03-31 13:37:46 +0530

    react hook form coding

--- star-rating-app/src/components/Login.jsx
+++ star-rating-app/src/components/Login.jsx
@@ -6,20 +6,23 @@ const Login = () => {
 
   const { register, handleSubmit } = useForm();
 
-  
+  const onFormSubmit = (data) => {
+    alert(JSON.stringify(data));
+  }
 
   return (
     <div className="text-center border-blue-500 border w-6/12 m-auto">
       <fieldset className="fieldset w-xs bg-base-100 border border-blue-300 p-4 rounded-box m-auto">
         <legend className="fieldset-legend">Login</legend>
-
+        <form action="" onSubmit={handleSubmit(onFormSubmit)}>
         <label className="fieldset-label">Email</label>
         <input type="email" className="input" placeholder="Email" {...register("email")} />
 
         <label className="fieldset-label">Password</label>
         <input type="password" className="input" placeholder="Password" {...register("password")} />
 
-        <button className="btn btn-neutral mt-4">Login</button>
+        <button className="btn btn-neutral mt-4" type="submit" >Login</button>
+        </form>
       </fieldset>
     </div>
   );
