e0f690045421fa31a8493b886128583d9a0ae604
Vishal Rathod <vrathod591@gmail.com>
2025-04-05 01:44:10 +0530

    Todo app coding

--- star-rating-app/src/App.jsx
+++ star-rating-app/src/App.jsx
@@ -1,17 +1,20 @@
 import StarRatings from "./components/StarRatings";
 import Counter from "./components/Counter";
-import Login from "./components/Login";
 import { lazy, Suspense } from "react";
 
 function App() {
   const Login = lazy(() => import("./components/Login"));
+  const Todo = lazy(()=> import("./components/Todo"));
 
   return (
     <>
       <StarRatings />
       <Counter />
-      <Suspense fallback="Loading...!">
+      <Suspense fallback="Login page Loading...!" className='text-2xl text-center font-bold'>
         <Login />
+      </Suspense>
+      <Suspense fallback="Todo App Loading...!" className='text-2xl text-center font-bold'>
+        <Todo/>
       </Suspense>
     </>
   );
---
+++ star-rating-app/src/components/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div className='bg-fuchsia-300 w-6/12 my-8 mx-auto border-2 border-fuchsia-600 text-black text-center p-4'>
+        <h1>My Todo App</h1>
+    </div>
+  )
+}
+
+export default Todo;
