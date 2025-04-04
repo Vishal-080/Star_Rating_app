d0a4dc01966290f65ac10bbaef059d7183ddcaed
Vishal Rathod <vrathod591@gmail.com>
2025-04-05 01:48:55 +0530

    Todo app coding

--- star-rating-app/src/components/Todo.jsx
+++ star-rating-app/src/components/Todo.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Todo = () => {
   return (
-    <div className='bg-fuchsia-300 w-6/12 my-8 mx-auto border-2 border-fuchsia-600 text-black text-center p-4'>
-        <h1>My Todo App</h1>
+    <div className="bg-fuchsia-300 w-6/12 my-8 mx-auto border-2 border-fuchsia-600 text-black text-center p-4">
+      <h1>My Todo App</h1>
+      <input type="text" placeholder="Enter Your Task Here..." />
+      <button>Add Todo</button>
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
 export default Todo;
