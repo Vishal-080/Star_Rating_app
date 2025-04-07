fca0a79b6049f480d38288d1ad9dea25fd09c189
Vishal Rathod <vrathod591@gmail.com>
2025-04-07 20:34:25 +0530

    App component

--- star-rating-app/src/App.jsx
+++ star-rating-app/src/App.jsx
@@ -4,17 +4,23 @@ import { lazy, Suspense } from "react";
 
 function App() {
   const Login = lazy(() => import("./components/Login"));
-  const Todo = lazy(()=> import("./components/Todo"));
+  const Todo = lazy(() => import("./components/Todo"));
 
   return (
     <>
       <StarRatings />
       <Counter />
-      <Suspense fallback="Login page Loading...!" className='text-2xl text-center font-bold'>
+      <Suspense
+        fallback="Login page Loading...!"
+        className="text-2xl text-center font-bold"
+      >
         <Login />
       </Suspense>
-      <Suspense fallback="Todo App Loading...!" className='text-2xl text-center font-bold'>
-        <Todo/>
+      <Suspense
+        fallback="Todo App Loading...!"
+        className="text-2xl text-center font-bold"
+      >
+        <Todo />
       </Suspense>
     </>
   );
