befc45adb830eaaae2a23bf22e2f0ca370796c49
Vishal Rathod <vrathod591@gmail.com>
2025-04-12 01:46:17 +0530

    Timer app

--- star-rating-app/src/components/Timer.jsx
+++ star-rating-app/src/components/Timer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 const Timer = () => {
+
+  const [hr, setHr] = useState(10);
+  const [min, setMin] = useState(10);
+  const [sec, setSec] = useState(10);
+
+
+
   return (
     <div>
       <div>
         <h1>Timer</h1>
+        <h3>{hr} : {min} :{sec}</h3>
       </div>
       <div>
         <button>Reset</button>
