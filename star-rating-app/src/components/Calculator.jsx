58bbde5c90f873a8606ed3358e54d68dfd6059e4
Vishal Rathod <vrathod591@gmail.com>
2025-04-15 01:32:56 +0530

    Calculator coding...

--- star-rating-app/src/components/Calculator.jsx
+++ star-rating-app/src/components/Calculator.jsx
@@ -1,39 +1,169 @@
 import React from "react";
+import { useState } from "react";
 
 const Calculator = () => {
+  const [data, setData] = useState("");
+  const handleBtnClick = (e) => {
+    setData(data.concat(e.target.value));
+  };
+
   return (
     <>
       <div className="w-6/12 m-auto my-8 bg-emerald-900 border p-4">
         <div className="bg-emerald-100 w-9/12 m-auto p-4">
-          <input type="text"  placeholder="0" className="w-full text-right font-bold text-2xl px-2 text-black" disabled/> 
+          <input
+            type="text"
+            placeholder="0"
+            className="w-full text-right font-bold text-2xl px-2 text-black"
+            disabled
+            value={data}
+          />
         </div>
-        <div className="bg-emerald-600 w-9/12 m-auto p-4"> 
+        <div className="bg-emerald-600 w-9/12 m-auto p-4 flex flex-wrap">
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="("
+          >
+            (
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value=")"
+          >
+            )
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="%"
+          >
+            %
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="AC"
+          >
+            AC
+          </button>
 
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="(">(</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value=")">)</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="%">%</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="AC">AC</button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="7"
+          >
+            7
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="8"
+          >
+            8
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="9"
+          >
+            9
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="*"
+          >
+            *
+          </button>
 
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="7">7</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="8">8</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="9">9</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="*">*</button>
-          
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="4">4</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="5">5</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="6">6</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="-">-</button>
-          
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="1">1</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="2">2</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="3">3</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="+">+</button>
-          
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="0">0</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="Back">Back</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="=">=</button>
-          <button className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none" value="/">/</button>
-           
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="4"
+          >
+            4
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="5"
+          >
+            5
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="6"
+          >
+            6
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="-"
+          >
+            -
+          </button>
+
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="1"
+          >
+            1
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="2"
+          >
+            2
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="3"
+          >
+            3
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="+"
+          >
+            +
+          </button>
+
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="0"
+          >
+            0
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="Back"
+          >
+            Back
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="="
+          >
+            =
+          </button>
+          <button
+            className="cursor-pointer text-center w-1/5 m-2 p-2 text-xl font-bold border rounded-lg hover:bg-emerald-300 hover:text-black hover:border-none"
+            onClick={handleBtnClick}
+            value="/"
+          >
+            /
+          </button>
         </div>
       </div>
     </>
