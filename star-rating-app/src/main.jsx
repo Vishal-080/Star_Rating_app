e0ab2ee22470be962317742d9767b6d46841dba3
Vishal Rathod <vrathod591@gmail.com>
2025-04-19 01:53:29 +0530

    main component

--- star-rating-app/src/main.jsx
+++ star-rating-app/src/main.jsx
@@ -7,8 +7,6 @@ import { ToastContainer } from "react-toastify";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <ToastContainer
-      position="top-right"
-    />
+    <ToastContainer position="top-right" />
   </StrictMode>
 );
